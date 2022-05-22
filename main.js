00cfa1fc65c3e855e5ef4535c921ce40828db426
HyunJoo Kim <hyunjoogo@tuneit.io>
2022-05-22 22:07:00 +0900

    prettier 및 eslint 적용

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ const lists = document.querySelectorAll('.list');
 
 let dragItem = null;
 
-
 function dragStart() {
   console.log('drag started');
   dragItem = this; // 여기서 this는 이벤트 대상
-  setTimeout(() => this.className = "invisible", 0);
+  setTimeout(() => this.className = 'invisible', 0);
 }
 
 function dragEnd() {
   console.log('drag Ended');
-  this.className = "card";
+  this.className = 'card';
   dragItem = null;
 }
 
@@ -31,18 +30,16 @@ function dragLeave() {
 
 function dragDrop() {
   console.log('drag dropped');
-  const listContents = this.querySelector(".list_contents");
+  const listContents = this.querySelector('.list_contents');
   listContents.append(dragItem);
-
 }
 
-
-cards.forEach(card => {
+cards.forEach((card) => {
   card.addEventListener('dragstart', dragStart);
   card.addEventListener('dragend', dragEnd);
 });
 
-lists.forEach(list => {
+lists.forEach((list) => {
   list.addEventListener('dragover', dragOver);
   list.addEventListener('dragenter', dragEnter);
   list.addEventListener('dragleave', dragLeave);
